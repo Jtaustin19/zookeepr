86b00bff91d256a4719d7996738e0b6351321623
Josh <jtaustin19@icloud.com>
2021-02-22 22:45:06 -0600

    added 404

--- server.js
+++ server.js
@@ -55,8 +55,12 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, anaimals);
-    res.json(result);
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
